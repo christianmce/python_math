506f46800a80dcdbbbdc0ec2b1ee51d1b1a2746d
ChristianMCE <cmce@unach.mx>
2019-09-12 23:15:50 -0500

    Create parabola.py

---
+++ parabola.py
@@ -0,0 +1,20 @@
+import numpy as np
+from matplotlib import pyplot as plt
+
+def f(x):
+    return x * x
+
+x = np.arange(-4,4,1/40.)
+plt.plot(x,f(x))
+
+
+section = np.arange(-4, 0, 1/20.)
+plt.fill_between(section,f(section))
+
+
+plt.axhline(0,color="black")
+plt.axvline(0,color="black")
+plt.xlim(-11,11)
+plt.ylim(-11,11)
+plt.grid()
+plt.show()
