e8418661bd64acd1029dadf4d5e759317f66ed06
ChristianMCE <cmce@unach.mx>
2022-11-07 14:54:42 -0600

    Update and rename histograma.py to graficar_histograma.py

--- graficar_histograma.py
+++ graficar_histograma.py
@@ -1,3 +1,6 @@
+# Autor: Dr. Christian Mauricio Castillo Estrada
+# Universidad Autónoma de Chiapas - LIDTS Facultad de Negocios
+
 import matplotlib.pyplot as plot
 
 edades = [12, 15, 13, 12, 18, 20, 19, 20, 13, 12, 13, 17, 15, 16, 13, 14, 13, 17, 19]
@@ -5,7 +8,7 @@ edades = [12, 15, 13, 12, 18, 20, 19, 20, 13, 12, 13, 17, 15, 16, 13, 14, 13, 17
 intervalos = range(min(edades), max(edades) + 2) #calculamos los extremos de los intervalos
 
 plot.hist(x=edades, bins=intervalos, color='#F2AB6D', rwidth=0.85)
-plot.title('Histograma de edades - matplotlib - codigopiton.com')
+plot.title('Histograma de edades estudiantes UNACH')
 plot.xlabel('Edades')
 plot.ylabel('Frecuencia')
 plot.xticks(intervalos)
