10bf79a226cb33dd052190d2d9820719d5dfd78b
ChristianMCE <cmce@unach.mx>
2022-11-07 14:52:34 -0600

    Create histograma.py

---
+++ histograma.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plot
+
+edades = [12, 15, 13, 12, 18, 20, 19, 20, 13, 12, 13, 17, 15, 16, 13, 14, 13, 17, 19]
+
+intervalos = range(min(edades), max(edades) + 2) #calculamos los extremos de los intervalos
+
+plot.hist(x=edades, bins=intervalos, color='#F2AB6D', rwidth=0.85)
+plot.title('Histograma de edades - matplotlib - codigopiton.com')
+plot.xlabel('Edades')
+plot.ylabel('Frecuencia')
+plot.xticks(intervalos)
+
+plot.show() #dibujamos el histograma
+
+
